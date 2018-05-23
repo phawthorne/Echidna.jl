5b9a536ac139ba5206dbd6836584f63b85bb3d40
Peter Hawthorne <phawthorne@umn.edu>
2018-05-23 15:43:32 -0500

    move some code to match platypus arrangement

--- core.jl
+++ core.jl
@@ -1,6 +1,7 @@
 import Base.copy
 include("var_types.jl")
 
+
 struct Problem
     nobjs::Int64
     directions::Vector{Bool}
@@ -8,6 +9,7 @@ struct Problem
     var_types::Vector{MOGA_Type}
 end
 
+# Solutions
 mutable struct Solution
     problem::Problem
     x::Vector{Float64}
@@ -17,11 +19,22 @@ mutable struct Solution
     rank::Int64
 end
 
+function copy(s::Solution)
+    return Solution(
+        s.problem,
+        copy(s.x),
+        s.evaluated,
+        copy(s.objectives),
+        s.crowding_distance,
+        s.rank
+    )
+end
+
+# Archives
 mutable struct Archive
     dominance::Function
     solutions::Vector{Solution}
 end
-
 
 function insert_solution!(archive::Archive, solution::Solution)
     flags = [archive.dominance(arch_sol, solution) for
@@ -45,6 +58,7 @@ function insert_solutions!(archive::Archive, solutions::Vector{Solution})
 end
 
 
+# Helper functions
 """
     compare_pareto_dominance(s1, s2)
 
@@ -89,13 +103,85 @@ function compare_pareto_dominance(s1, s2)
 
 end
 
-function copy(s::Solution)
-    return Solution(
-        s.problem,
-        copy(s.x),
-        s.evaluated,
-        copy(s.objectives),
-        s.crowding_distance,
-        s.rank
-    )
+
+function nondominated(solutions)
+    archive = Archive()
+    insert_solutions!(archive, solutions)
+    return archive.solutions
+end
+
+function nondominated_sort(solutions)
+    rank::Int64 = 0
+
+    for s in solutions
+        s.rank = -1
+    end
+
+    while length(solutions) > 0
+        archive = Archive(compare_pareto_dominance, Vector{Solution}())
+        insert_solutions!(archive, solutions)
+
+        for s in archive.solutions
+            s.rank = rank
+        end
+
+        crowding_distance(archive.solutions)
+        solutions = [s for s in solutions if s.rank == -1]
+        rank += 1
+    end
+end
+
+function nondominated_cmp(x::Solution, y::Solution)
+    if x.rank == y.rank
+        if -x.crowding_distance < -y.crowding_distance
+            return -1
+        elseif -x.crowding_distance > -y.crowding_distance
+            return 1
+        else
+            return 0
+        end
+    else
+        if x.rank < y.rank
+            return -1
+        elseif x.rank > y.rank
+            return 1
+        else
+            return 0
+        end
+    end
+end
+
+function crowding_distance(solutions)
+    for s in solutions
+        s.crowding_distance = 0.0
+    end
+
+    nobjs = length(solutions[1].objectives)
+
+    if length(solutions) < 3
+        for s in solutions
+            s.crowding_distance[1] = Inf
+        end
+    else
+        # main case
+        for i in 1:nobjs
+            sorted_solutions = sort!(solutions, by=s->s.objectives[i])
+            min_value = sorted_solutions[1].objectives[i]
+            max_value = sorted_solutions[end].objectives[i]
+
+            sorted_solutions[1].crowding_distance += Inf
+            sorted_solutions[end].crowding_distance += Inf
+
+            for j in 2:length(solutions)-1
+                if max_value - min_value < eps(Float64)
+                    sorted_solutions[j].crowding_distance = Inf
+                else
+                    diff = sorted_solutions[j+1].objectives[i] -
+                           sorted_solutions[j-1].objectives[i]
+                    sorted_solutions[j].crowding_distance += diff /
+                                                (max_value - min_value)
+                end
+            end
+        end
+    end
 end
