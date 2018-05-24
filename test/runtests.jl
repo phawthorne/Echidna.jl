ec3e8f8863ce6bb6a855a8e099850830e9a98bec
Unknown <hawt0010@umn.edu>
2018-05-24 08:26:22 -0500

    minimal test

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,5 +1,9 @@
-using echidna
+using Echidna
 using Base.Test
 
 # write your own tests here
-@test 1 == 2
+lb = 4.0
+ub = 9.0
+mr = MOGA_Real(lb, ub)
+res = [rand(mr) for i in 1:1000]
+@test all(r>lb for r in res) && all(r<ub for r in res)
