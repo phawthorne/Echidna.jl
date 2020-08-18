fdab56659df2171a3df629ba24e7644ecf5ca905
zeptodoctor <44736852+zeptodoctor@users.noreply.github.com>
2020-08-18 17:43:40 +0000

    build based on 9488941

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"#Introduction","page":"Introduction","title":"Introduction","text":"","category":"section"},{"location":"","page":"Introduction","title":"Introduction","text":"Echidna provides implementations of NSGA-II and NSGA-III evolutionary algorithms heavily inspired by Platypus.","category":"page"}]
+}
